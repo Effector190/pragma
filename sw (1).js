c64967e2f58037326bcca5b555f30fd8aa23eb00
Effector190 <ddzonson461@gmail.com>
2026-03-09 01:10:02 +0400

    Add files via upload

---
+++ sw (1).js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'pragma-cache-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/pragma_app.html',
+  '/icon-512.png',
+  'https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,600;1,300;1,400&family=Inter:wght@300;400;500&display=swap'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
